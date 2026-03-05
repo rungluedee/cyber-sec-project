0cf5350e4cbf54232aabde458e8459f9d09cf6f5
HotSaucesausage <Kephuripat@gmail.com>
2026-03-05 17:05:48 +0700

    feat: add login attempt log

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
 def login(username, password):
-    """Basic login function"""
-    if username == "admin" and \
-       password == "secure_pw":
-        return True
-    return False
+    print("Login attempt detected")
+    if username == "admin":
+        return "Login Success"
+    return "Login Failed"
